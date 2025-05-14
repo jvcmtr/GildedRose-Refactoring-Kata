260c7f7af9841bb38907f6f8f5a8ea280bb3c2c0
jvcmtr <jvictorcmtr@gmail.com>
2025-05-14 02:45:59 -0300

    Added conjured item test

--- csharp.NUnit/GildedRoseTests/GildedRoseTest.cs
+++ csharp.NUnit/GildedRoseTests/GildedRoseTest.cs
@@ -7,6 +7,26 @@ namespace GildedRoseTests;
 public class GildedRoseTest
 {
     [Test]
+    public void ConjuredItem()
+    {
+        var initialQuality = 50;
+        var initialSellin = 10;
+        
+        var defaultItem = new DefaultItem(){ Name = "default", SellIn = initialSellin, Quality = initialQuality };
+        var conjuredItem = new ConjuredItem(){ Name = "conjured", SellIn = initialSellin, Quality = initialQuality } ;
+        
+        var items = new List<IItem> { defaultItem, conjuredItem};
+        var app = new GildedRose(items);
+        
+        for (int i = 0; i < 10; i++){
+            app.UpdateQuality();
+        }
+
+        var ratio = (initialQuality - conjuredItem.Quality) / (decimal) (initialQuality - defaultItem.Quality) ;
+        Assert.That(ratio, Is.EqualTo(2));
+    }
+
+    [Test]
     public void Foo()
     {
         var items = new List<IItem> { new DefaultItem(){ Name = "foo", SellIn = 0, Quality = 0 } };
